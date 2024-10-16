d1126f8bb5beb22e9ddb044b225411239f61ed1d
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-10-16 16:46:34 +0700

    BTVN

---
+++ Day13/BTVN/main.js
@@ -0,0 +1,27 @@
+const todos = [
+	{
+		id: 1,
+		title: "task 1",
+		description: "mo ta cong viec 1",
+		status: false,
+	},
+
+	{
+		id: 2,
+		title: "task 2",
+		description: "mo ta cong viec 2",
+		status: false,
+	},
+	{
+		id: 3,
+		title: "task 3",
+		description: "mo ta cong viec 3",
+		status: false,
+	},
+];
+
+/**
+ * - Xây dựng giao diện hiển thị việc cần làm dạng bảng.
+ * - Viết các hàm chức năng cho form và các nút bấm: Thêm, cập nhật trạng thái, sửa, xoá việc cần làm.
+ * - Khi nhấn vào tên của việc cần làm sẽ cập nhật trạng thái và màu sắc của title, ví dụ: "task 1 - completed" hoặc "task 2 - pending"
+ */
