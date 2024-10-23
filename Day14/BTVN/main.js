524d9764ffcbedc3f3b4a4b1205239a1e1b9af29
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-10-23 15:27:46 +0700

    Day 31: remove Todo

--- Day14/BTVN/main.js
+++ Day14/BTVN/main.js
@@ -1,13 +1,29 @@
 const tbody = document.getElementById("tbody");
+const todoForm = document.getElementById("todoForm");
+const titleElement = document.getElementById("title");
+const descriptionElement = document.getElementById("description");
 
-fetch("http://localhost:3000/todos")
+const url = "http://localhost:3000/todos";
+
+let todos = [];
+
+fetch(url)
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data);
 		// xu ly logic
-		data.forEach((item) => {
-			let tr = document.createElement("tr");
-			tr.innerHTML = `
+		todos.push(...data);
+		renderTodos(todos);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function renderTodos(todos) {
+	tbody.innerText = "";
+	todos.forEach((item) => {
+		let tr = document.createElement("tr");
+		tr.innerHTML = `
         <td>${item.id}</td>
         <td>${item.title}</td>
         <td>${item.description}</td>
@@ -16,17 +32,55 @@ fetch("http://localhost:3000/todos")
           <button onclick="updateTodo(${item.id})">Update</button>
         </td>
       `;
-			tbody.appendChild(tr);
-		});
-	})
-	.catch((err) => {
-		console.log(err);
+		tbody.appendChild(tr);
 	});
+}
 
 function removeTodo(id) {
 	console.log(id);
+
+	confirm("Are you sure?") &&
+		fetch(`${url}/${id}`, {
+			method: "DELETE",
+		})
+			.then(() => {
+				console.log("Xoa thanh cong");
+				todos = todos.filter((item) => item.id !== id);
+				renderTodos(todos);
+			})
+			.catch((err) => console.log(err));
+	// http://localhost:3000/todos/:id
 }
 
 function updateTodo(id) {
 	console.log(id);
 }
+
+function handleTodo(event) {
+	event.preventDefault();
+	console.log("add");
+	const todo = {
+		title: titleElement.value,
+		description: descriptionElement.value,
+		status: false,
+	};
+	fetch("http://localhost:3000/todos", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(todo),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			todos.push(...data);
+			renderTodos(todos);
+			console.log(`Them thanh cong, `, data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+todoForm.addEventListener("submit", handleTodo);
