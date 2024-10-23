cada6b0292b0198350de15773dc858ea1b0594a5
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-10-23 16:05:05 +0700

    Day 32: Update todo

--- Day14/BTVN/main.js
+++ Day14/BTVN/main.js
@@ -2,10 +2,14 @@ const tbody = document.getElementById("tbody");
 const todoForm = document.getElementById("todoForm");
 const titleElement = document.getElementById("title");
 const descriptionElement = document.getElementById("description");
+const addBtn = document.getElementById("add-btn");
 
 const url = "http://localhost:3000/todos";
 
 let todos = [];
+let todoEditing = {
+	id: null,
+};
 
 fetch(url)
 	.then((response) => response.json())
@@ -37,8 +41,6 @@ function renderTodos(todos) {
 }
 
 function removeTodo(id) {
-	console.log(id);
-
 	confirm("Are you sure?") &&
 		fetch(`${url}/${id}`, {
 			method: "DELETE",
@@ -49,38 +51,67 @@ function removeTodo(id) {
 				renderTodos(todos);
 			})
 			.catch((err) => console.log(err));
-	// http://localhost:3000/todos/:id
 }
 
 function updateTodo(id) {
-	console.log(id);
+	todoEditing = todos.find((item) => item.id === id);
+	titleElement.value = todoEditing.title;
+	description.value = todoEditing.description;
+	addBtn.innerText = "Update";
 }
 
 function handleTodo(event) {
 	event.preventDefault();
-	console.log("add");
-	const todo = {
-		title: titleElement.value,
-		description: descriptionElement.value,
-		status: false,
-	};
-	fetch("http://localhost:3000/todos", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(todo),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-			todos.push(...data);
+	if (todoEditing.id) {
+		// logic edit
+		const todo = {
+			title: titleElement.value,
+			description: descriptionElement.value,
+			status: todoEditing.status,
+		};
+		fetch(`${url}/${todoEditing.id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(todo),
+		}).then(() => {
+			// Cach 1:
+			todos = todos.map((item) => {
+				if (item.id === todoEditing.id) {
+					return todoEditing;
+				}
+
+				// Cach 2: Goi server lay lai danh sach viec can lam
+				return item;
+			});
 			renderTodos(todos);
-			console.log(`Them thanh cong, `, data);
-		})
-		.catch((err) => {
-			console.log(err);
 		});
+	} else {
+		// logic add
+		const todo = {
+			title: titleElement.value,
+			description: descriptionElement.value,
+			status: false,
+		};
+		fetch("http://localhost:3000/todos", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(todo),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				todos.push(...data);
+				renderTodos(todos);
+				console.log(`Them thanh cong, `, data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 }
 
 todoForm.addEventListener("submit", handleTodo);
