adc96ebaf2c19ac945e9cfc0db02645ad6d9e3c9
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-11-03 15:09:13 +0700

    add detail page

--- projects-k10/js/main.js
+++ projects-k10/js/main.js
@@ -1,34 +1,15 @@
 import { getAll, getById, removeById, updateById } from "./services.js";
-import { getParams } from "./utils.js";
+import { getParams, render } from "./utils.js";
 
 const { products } = await getAll("products");
 console.log(products);
 
 const hotSaleSection = document.getElementById("hot-sale");
 
-function render(target, datas) {
-	datas.forEach((item) => {
-		const productElement = document.createElement("div");
-		productElement.innerHTML = /*html*/ `
-        <div class="product-card">
-          <a href='/product-detail.html?id=${item.id}'><img src="${item.thumbnail}" alt="${item.title}" /></a>
-          <div class="product-infor">
-            <h2>${item.title}</h2>
-            <div>Giá: ${item.price}</div>
-            <p>Mô tả: ${item.description}</p>
-            <button>Xem chi tiết</button>
-          </div>
-        </div>
-    `;
-		target && target.appendChild(productElement);
-	});
-}
-
 render(hotSaleSection, products);
 // render(hotSaleSection, products);
 
 const productId = getParams("id");
-console.log(productId);
 
 const product = await getById("products", productId);
 
--- projects-k10/js/utils.js
+++ projects-k10/js/utils.js
@@ -3,3 +3,21 @@ export function getParams(key) {
 	const params = new URLSearchParams(window.location.search);
 	return params.get(key);
 }
+
+export function render(target, datas) {
+	datas.forEach((item) => {
+		const productElement = document.createElement("div");
+		productElement.innerHTML = /*html*/ `
+        <div class="product-card">
+          <a href='/product-detail.html?id=${item.id}'><img src="${item.thumbnail}" alt="${item.title}" /></a>
+          <div class="product-infor">
+            <h2>${item.title}</h2>
+            <div>Giá: ${item.price}</div>
+            <p>Mô tả: ${item.description}</p>
+            <button>Xem chi tiết</button>
+          </div>
+        </div>
+    `;
+		target && target.appendChild(productElement);
+	});
+}
