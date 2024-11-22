8a9950cdf3bb6b8b89a5822853d3cdc501ec47db
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-11-22 15:14:54 +0700

    Day_02

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import styles from "./App.module.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
 
-function Welcome(props) {
-	// props = propeties
-	// console.log(styles);
-	return <h1 style={{ backgroundColor: "red", color: "white" }}>Hello {props.name}</h1>;
+function Button(props) {
+	console.log(props);
+	return <button className={props.variant}>{props.children}</button>;
 }
 
 function App() {
-	const username = "hoangnm";
 	return (
 		<>
 			<Header />
-			<Welcome name={username} />
-			<Welcome name="Huy" />
-			<button id={styles.btn}>Sign In</button>
-			<Welcome name="Giang" />
+			<Button variant="btn btn-primary">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, deserunt!</p>
+			</Button>
+			<Button variant="btn btn-secondary">Btn Secondary</Button>
+			<Button variant="btn btn-warning">Btn Warning</Button>
+			<Button variant="btn btn-danger">Btn Danger</Button>
+			<Button variant="btn btn-link">Btn Link</Button>
 			<Footer />
 		</>
 	);
