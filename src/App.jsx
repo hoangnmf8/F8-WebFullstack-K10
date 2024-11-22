4324c999282158eacc66d4026dcb7f697a6e62c7
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-11-22 14:52:58 +0700

    Day_02: style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Courses from "./components/Courses";
-import Footer from "./components/Footer";
+import styles from "./App.module.scss";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
 
+function Welcome(props) {
+	// props = propeties
+	// console.log(styles);
+	return <h1 style={{ backgroundColor: "red", color: "white" }}>Hello {props.name}</h1>;
+}
+
 function App() {
+	const username = "hoangnm";
 	return (
 		<>
 			<Header />
-			<Courses />
+			<Welcome name={username} />
+			<Welcome name="Huy" />
+			<button id={styles.btn}>Sign In</button>
+			<Welcome name="Giang" />
 			<Footer />
 		</>
 	);
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-	return <div>Copyright by F8</div>;
-};
-
-export default Footer;
