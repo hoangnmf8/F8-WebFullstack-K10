cbcff1080c515ce3268b56d46fc2d137f6b3139d
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-11-24 15:15:07 +0700

    Day_03: cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,57 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/Header";
 
 /**
- * 1. useEffect có dependences là mảng rỗng
+ * 1. useEffect có dependences là mảng rỗng: callback chỉ gọi 1 lần ngay sau lần render đầu tiên.
+ * 2. useEffect có dependences khác mảng rỗng: callback gọi 1 lần ngay sau lần render đầu tiên & được gọi khi sự phụ thuộc thay đổi.
+ * 3. useEffect không có dependences: callback được gọi 1 lần sau lần render đầu tiên và sau mỗi lần re-render.
  *
  */
 
-function ComponentA() {
+function ProductList() {
 	useEffect(() => {
-		console.log("trong useEffect cua component A");
-	});
-	return <h2>Noi dung component A</h2>;
+		console.log("trong useEffect");
+		return () => {
+			console.log("trong return cua useEffect");
+			// function dọn dẹp ngay trước khi component ProductList được unmounting
+		};
+	}, []);
+	return <h1>Danh sach san pham</h1>;
 }
 
 function App() {
-	const [show, setShow] = useState(false);
-	useEffect(() => {
-		console.log("trong useEffect cua component App");
-	}, [show]);
+	const [count, setCount] = useState(0);
+	const [showProducts, setShowProducts] = useState(false);
+
+	// useEffect(() => {
+	// 	console.log("trong useEffect");
+	// 	return () => {
+	// 		console.log("trong return của useEffect");
+	// 	};
+	// }, [count]);
+
+	// console.log("trong App");
+
 	return (
 		<>
 			<Header />
-			<h1>Hello</h1>
-			<button className="btn btn-primary" onClick={() => setShow(!show)}>
-				Toggle
+			<button
+				onClick={() => {
+					setCount(count + 1);
+				}}
+			>
+				Tăng
 			</button>
+			{count}
+
+			<button
+				onClick={() => {
+					setShowProducts(!showProducts);
+				}}
+			>
+				Toogle Products
+			</button>
+
+			{showProducts && <ProductList />}
 			<Footer />
 		</>
 	);
