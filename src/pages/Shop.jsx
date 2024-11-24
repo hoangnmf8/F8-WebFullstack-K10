596eb9788bb1e6e817edeccf797f18ac63209c99
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-11-24 16:52:00 +0700

    Day_03: search

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 
 const Shop = () => {
 	const [products, setProducts] = useState([]);
-	const [limit, setLimit] = useState(10);
+	const [limit, setLimit] = useState(3);
 	const [skip, setSkip] = useState(0);
+	const [page, setPage] = useState(1);
 	useEffect(() => {
-		fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
+		fetch(
+			limit === "all" ? `https://dummyjson.com/products` : `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
+		)
 			.then((res) => res.json())
 			.then(({ products }) => {
 				console.log(products);
@@ -16,15 +19,25 @@ const Shop = () => {
 	const handleSelectLimit = (e) => {
 		const selectedLimit = e.target.value;
 		console.log(selectedLimit);
-		if (selectedLimit === "all") {
-			console.log("get All");
-		} else {
-			setLimit(selectedLimit);
-		}
+		setLimit(selectedLimit);
 	};
+
+	const handlePrev = () => {
+		setSkip();
+	};
+
+	const handleNext = () => {
+		setSkip();
+	};
+
+	const handleSearch = (e) => {
+		console.log(e.target.value);
+	};
+
 	return (
 		<div>
 			<h2>Danh sach san pham</h2>
+			<input type="text" onChange={(e) => handleSearch(e)} />
 			<select onChange={(e) => handleSelectLimit(e)}>
 				<option value={10}>Hiển thị 10 sản phẩm</option>
 				<option value={20}>Hiển thị 20 sản phẩm</option>
@@ -43,6 +56,12 @@ const Shop = () => {
 				</div>
 			))}
 			{/* nut bam phan trang */}
+			<button className="btn btn-primary" onClick={handlePrev}>
+				Prev
+			</button>
+			<button className="btn btn-primary" onClick={handleNext}>
+				Next
+			</button>
 		</div>
 	);
 };
