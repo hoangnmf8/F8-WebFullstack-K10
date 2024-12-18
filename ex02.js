ce5a88834d1830e7c971a63cb04225a14f3c6abe
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-12-18 16:09:27 +0700

    Typescript

---
+++ ex02.js
@@ -0,0 +1,11 @@
+"use strict";
+let productA = { title: "tivi", price: 200 };
+var ProductSize;
+(function (ProductSize) {
+    ProductSize[ProductSize["S"] = 0] = "S";
+    ProductSize[ProductSize["M"] = 1] = "M";
+    ProductSize[ProductSize["L"] = 2] = "L";
+    ProductSize[ProductSize["XL"] = 3] = "XL";
+    ProductSize[ProductSize["XXL"] = 4] = "XXL";
+})(ProductSize || (ProductSize = {}));
+//# sourceMappingURL=ex02.js.map
